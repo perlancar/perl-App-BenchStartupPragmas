0a968aecd2ec50036528539504002acf25aec32f
perlancar (@pc-home) <perlancar@gmail.com>
2015-06-14 11:55:21 +0700

    Initial commit

---
+++ lib/App/BenchStartupPragmas.pm
@@ -0,0 +1,11 @@
+package App::BenchStartupPragmas;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Benchmark startup time of various Perl pragmas
+
+=head1 SYNOPSIS
+
+See the included script L<bench-startup-pragmas>.
